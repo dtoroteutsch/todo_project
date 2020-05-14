b032323689574aa28649fbd9f0d6a6349e5d5f67
Diego <toro.teutsch.d@gmail.com>
2020-05-14 12:35:03 -0400

    The methods index and hello has been defined

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index'
+
+@app.route('/hello')
+def hello():
+    return 'Hello world - Flask'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)

f9c4593b3dff70ff5094e6e47c8b418367c55e5e
Diego <toro.teutsch.d@gmail.com>
2020-05-14 22:57:35 -0400

    Changing cookies for session variables

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = '1Nw4rdH33lfl1p'
 
 todos = ['Buy coffee', 'Study Flask', 'Eat many things']
 
@@ -14,12 +16,12 @@ def page_not_found(error):
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'title': 'Hello',
         'user_ip': user_ip,

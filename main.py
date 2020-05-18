f008192d44be518252f727286f2d1eede77389e1
Diego <toro.teutsch.d@gmail.com>
2020-05-17 21:17:51 -0400

    Implementing blueprints for login form and testing that functionality

--- main.py
+++ main.py
@@ -31,22 +31,14 @@ def index():
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
 
     context = {
         'title': 'Hello',
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form,
         'username': username
     }
-
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
-        flash('User logged successfully')
-        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
 
